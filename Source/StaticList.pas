8c759539ebeb6d3c789d91f9939ba5bd09195305
xxherooorehxx <carloramosmuras@gmail.com>
2017-02-21 20:06:39 +0100

    Agregado

---
+++ Source/StaticList.pas
@@ -0,0 +1,110 @@
+unit StaticList;
+	interface
+	const
+		MAX=100;
+		NULL=0;
+			
+	type
+		tnIngredient=string;
+		tQuantity=integer;
+		tGluten=boolean;
+		tMilk=boolean;
+		tAllergens= record
+			gluten:tGluten;
+			milk:tMilk;
+			end;
+		tItem= record
+			nIngredient:tnIngredient;
+			quantity:tQuantity;
+			allergens:tAllergens;
+			end;
+		tPosL= NULL..MAX;
+		tList= record 
+			data:array [1..MAX] of tItem;
+			endList: tPosL;
+			end;
+		
+	procedure createEmptyList(VAR L:tList);
+	function isEmptyList(L:tList):boolean;
+	function first(L:tList):tPosL;
+	function last(L:tList):tPosL;
+	function next(p:tPosL; L:tList):tPosL;
+	function previous(p:tPosL; L:tList):tPosL;
+	function insertItem(i:tItem; p:tPosL; L:tList):boolean;
+	procedure deleteAtPosition (p:tPosL; VAR L:tList);
+	function getItem (p:tPosL; L:tList):tItem;
+	procedure updateItem (VAR L:tList; p:tPosL; i:tItem);
+	function findItem (ing:tnIngredient; L:tList):tPosL;
+	
+	implementation
+	
+	procedure createEmptyList(VAR L:tList);
+	begin
+		L.endList:=NULL;
+	end;
+	
+	function isEmptyList(L:tList):boolean;
+	begin
+		isEmptyList:=(L.endList=NULL);
+	end;
+	
+	function first(L:tList):tPosL;
+	begin
+		first:=1;
+	end;
+	
+	function last(L:tList):tPosL;
+	begin
+		last:=L.endlist;
+	end;
+	
+	function next(p:tPosL; L:tList):tPosL;
+	begin
+		if p=last(L) then
+			next:=NULL
+		else 
+			p:=p+1;
+	end;
+	
+	function previous(p:tPosL; L:tList):tPosL;
+	begin
+		if p=first(L) then
+			previous:=NULL
+		else 
+			p:=p-1;
+	end;
+	
+	function insertItem(i:tItem; p:tPosL; L:tList):boolean;
+	begin
+	insertItem:=false;
+	end;
+	
+	procedure deleteAtPosition (p:tPosL; VAR L:tList);
+	begin
+		
+	end;
+	
+	function getItem (p:tPosL; L:tList):tItem;
+	begin
+		getItem:=L.data[p];
+	end;
+	
+	procedure updateItem (VAR L:tList; p:tPosL; i:tItem);
+	begin
+		L.data[p]:=i;		
+	end;
+	
+	function findItem (ing:tnIngredient; L:tList):tPosL;
+	var
+		p:tPosL;
+	begin
+		findItem:=NULL;
+		p:=first(L);
+		while ((findItem=NULL) AND (p <> NULL)) do
+		begin
+			if (L.data[p].nIngredient=ing) then
+				findItem:=p;
+			p:=next(p,L);
+		end;
+	end;
+end.
---
+++ Source/main.pas
@@ -0,0 +1,225 @@
+program main;
+
+uses list,crt,sysutils;
+
+function IntToStr(int:integer):string;
+begin
+	Str(int,IntToStr);
+end;
+
+function boolToString(bool:boolean):string;
+begin
+	if(bool)
+		then boolToString := 'true'
+		else boolToString := 'false';
+		
+end;
+procedure imprimirItem(item:tItem);
+begin
+	writeln('* Ingredient ',item.nIngredient,': ',item.quantity);
+	if(item.allergens.gluten)
+	then writeln('      Contains gluten');
+	
+	if(item.allergens.milk)
+	then writeln('      Contains milk');
+end;
+
+procedure imprimirLinea(linea1:string;linea2:string;linea3:string;linea4:string;linea5:string);
+begin
+	writeln('**** ',linea1,' ',linea2,' ',linea3,' ',linea4,' ',linea5);
+end;
+
+procedure imprimirError(error1:string;error2:string;error3:string);
+begin
+	writeln('++++ ',error1,' ',error2,' ',error3);
+end;
+
+procedure newIngredient(nIngrediente:string;cant:integer;gluten:boolean;milk:boolean;var lista:tList);
+var
+	item:tItem;
+	sCant:string;
+begin
+	Str(cant,sCant);
+	imprimirLinea('Adding new ingredient:',nIngrediente,sCant,boolToString(gluten),boolToString(milk));
+	if(cant<=0)
+		then imprimirError('Error adding New:','Invalid','Quantity')
+		else begin
+			if(findItem(nIngrediente,lista)<>NULL)
+				then imprimirError('ERROR Adding New: Ingredient',nIngrediente,'already exists')
+				else begin
+					item.nIngredient := nIngrediente;
+					item.quantity := cant;
+					item.allergens.gluten := gluten;
+					item.allergens.milk := milk;
+					if NOT(insertItem(item,NULL,lista))
+						then imprimirLinea('WARNING: Out of memory','for','adding',nIngrediente,'ingredient');					
+				end;
+		end;	
+end;
+
+procedure Stock(filtro:boolean;minQuantity:integer;lista:tList);
+var
+	q:tPosL;
+	item:tItem;
+	printed:boolean;
+	gluten,milk,total:integer;
+	pgluten,pmilk:real;
+begin
+	gluten :=0; milk:=0; total:=0; pgluten := 0; pmilk := 0;
+	if(minQuantity<=0)
+	then imprimirError('ERROR in Stock:','invalid','quantity')
+	else begin
+	
+		if (isEmptyList(lista))
+		then
+			imprimirLinea('No','stock','available',' ',' ')
+		else begin
+			q:=first(lista);
+			if(filtro)
+			then begin
+				imprimirLinea('Stock(<',IntToStr(minQuantity),'):',' ',' ');
+				while(q<>NULL) do begin
+					item:=getItem(q,lista);
+					if(item.quantity<=minQuantity)
+					then 
+						printed:=true;
+						imprimirItem(item);
+						total:=total+1;
+					if(item.allergens.gluten)
+						then gluten:=gluten+1;
+					if(item.allergens.milk)
+						then milk:=milk+1;
+					
+					q:=next(q,lista);
+					end;
+			end			
+			else begin
+				imprimirLinea('Stock',':',' ',' ',' ');
+				while(q<>NULL) do begin
+					item:=getItem(q,lista);
+					imprimirItem(item);
+					total:=total+1;
+					if(item.allergens.gluten)
+						then gluten:=gluten+1;
+					if(item.allergens.milk)
+						then milk:=milk+1;
+					q:=next(q,lista);
+				end;
+			end;
+		
+		if(filtro)AND(NOT(printed))
+			then imprimirLinea('No','ingredients','below','the','threshold');
+		if(filtro)
+			then imprimirLinea('Number of ingredients','in stock (<',IntToStr(minQuantity),'):',IntToStr(total))
+			else imprimirLinea('Number of ingredients','in','stock',':',IntToStr(total));
+		pgluten:=(gluten/total)*100;
+		pmilk:=(milk/total)*100;
+		writeln('      ',pgluten:0:1,'% contains gluten');
+		writeln('      ',pmilk:0:1,'% contains milk');
+		
+		end;
+	end;
+end;
+
+
+procedure readTasks(taskFile:string);
+var
+   fileId:Text; //file identifier
+   code: string; //code for each task[N, M, R, S or A]
+   line:string; //each line in the file
+   name:string; //name of ingredient
+   quantity:integer; //quantity of the ingredient
+   allergen1: string; // string "true" or "false" to indicate if the ingredient contains gluten
+   allergen2: string; // string "true" or "false"to indicate if the ingredient contains milk
+   strquantity:string; //string to convert to quantity
+   withQuantity: boolean; //indicates the operation S includes a quantity
+   gluten: boolean; //indicates if the ingredient has gluten
+   milk: boolean;//indicates if the ingredient has milk
+   lista:tList;
+   //ADD VARIABLES IF YOU NEED THEM
+   	
+	
+begin
+   createEmptyList(lista);
+   {$i-}
+   assign(fileId, taskFile);
+   reset(fileId);
+   {$i+}
+   if (IOResult<>0) then begin
+   		writeln('**** Reading. Error when trying to open ', taskFile);
+   		halt(1);
+   end;
+
+  //Read all lines in file
+   While (not EOF(fileId))  do
+   begin
+      readln(fileId, line);
+      code:=trim(copy(line,1,2));
+      
+      //Select the proper arguments from each line of the file
+      case code[1] of
+		'N', 'n': begin 
+					name:=(trim(copy(line,3,12))); 
+					val(trim(copy(line,17,7)),quantity);				
+					allergen1:=trim(copy(line,23,6));
+					allergen2:=trim(copy(line,29,6));
+					//converts a string (true/false) to boolean
+					if not TryStrToBool(allergen1, gluten) then begin
+						writeln('**** Reading. Error reading data from ', taskFile);
+						halt(1);
+					end; 
+					if not TryStrToBool(allergen2, milk)then begin
+						writeln('**** Reading. Error reading data from ', taskFile);
+						halt(1);
+					end; 
+					newIngredient(name,quantity,gluten,milk,lista);
+				  end;
+		'M', 'm': begin 
+					name:=trim(copy(line,3,12)); 
+					val(trim(copy(line,17,7)),quantity);
+				  end;
+		'R', 'r': val(trim(copy(line,3,7)),quantity);
+		'A', 'a': begin 
+					allergen1:=trim(copy(line,3,6));
+					allergen2:=trim(copy(line,9,6));
+										//converts a string (true/false) to boolean
+					if not TryStrToBool(allergen1, gluten) then begin
+						writeln('**** Reading. Error reading data from ', taskFile);
+						halt(1);
+					end; 
+					if not TryStrToBool(allergen2, milk)then begin
+						writeln('**** Reading. Error reading data from ', taskFile);
+						halt(1);
+					end;  
+				  end;
+		'S', 's': begin 
+					strquantity:=trim(copy(line,3,7));
+					if (strquantity <>'') then begin
+						val(strquantity,quantity);
+						withQuantity:=true;
+					end
+					else withQuantity:=false;
+					Stock(withQuantity,quantity,lista);
+				  end;
+	end;
+		   
+
+	//EXECUTE TASK 
+    //...
+	
+	end; (*while*)
+
+end; 
+
+
+// ---------------------------------------------------------------------------------
+
+BEGIN
+
+	if (paramCount>0) then
+		readTasks(ParamStr(1))
+	else
+		readTasks('new.txt');
+
+
+END.
