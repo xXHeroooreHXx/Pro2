332ecfc574af00f9b9cebe4ba57c1958a972aa27
Malacrack <julip.c@hotmail.com>
2017-03-06 23:00:51 +0100

    Update DinamicList.pas
    + procedure createNode
    + function insertItem
    + procedure deleteAtPosition

--- Source/DinamicList.pas
+++ Source/DinamicList.pas
@@ -19,10 +19,10 @@ type
 		allergens:tAllergens;
 		end;
 	
-	tPosL= ^tNodo;
+	tPosL= ^tNode;
 
 	
-	tNodo = record
+	tNode = record
 		item:tItem;
 		next:tPosL;
 	end;
@@ -34,8 +34,9 @@ function first(L:tList):tPosL;
 function last(L:tList):tPosL;
 function next(p:tPosL; L:tList):tPosL;
 function previous(p:tPosL; L:tList):tPosL;
-//function insertItem(i:tItem; p:tPosL; L:tList):boolean;
-//procedure deleteAtPosition (p:tPosL; VAR L:tList);
+procedure createNode(d:tData;VAR newPos:tPosL);
+function insertItem(i:tItem; p:tPosL; L:tList):boolean;
+procedure deleteAtPosition (p:tPosL; VAR L:tList);
 function getItem (p:tPosL; L:tList):tItem;
 procedure updateItem (VAR L:tList; p:tPosL; i:tItem);
 function findItem (ing:tnIngredient; L:tList):tPosL;
@@ -44,26 +45,26 @@ function findItem (ing:tnIngredient; L:tList):tPosL;
 
 implementation	
 
-procedure createEmptyList(var l:tList);
+procedure createEmptyList(var L:tList);
 begin
-	l:=NULL;
+	L:=NULL;
 end;
 
-function isEmptyList(l:tList):boolean;
+function isEmptyList(L:tList):boolean;
 begin
-	isEmptyList:=(l=NULL)
+	isEmptyList:=(L=NULL)
 end;
 
-function first(l:tList):tPosL;
+function first(L:tList):tPosL;
 begin
-	first:=l;
+	first:=L;
 end;
 
-function last(l:tList):tPosL;
+function last(L:tList):tPosL;
 var
-p:tPosL;
+	p:tPosL;
 begin
-	p:=first(l);
+	p:=first(L);
 	while(p^.next <> NULL) do
 		p:=p^.next;
 	last:=p;
@@ -74,18 +75,83 @@ begin
 	next:=p^.next;
 end;
 
-function previous(p:tPosL; l:tList):tPosL;
+function previous(p:tPosL; L:tList):tPosL;
 var
 	q:tPosL;
 begin
 	if(p=l) then
 		previous:=NULL
 	else begin
-		q:=first(l);
+		q:=first(L);
 		while(q^.next <> p) do
 			q:=q^.next;
 		previous := q;
 	end;
+end;
+
+procedure createNode(d:tData;VAR newPos:tPosL);
+begin
+	newPos:=NULL;
+	new(newPos);
+	if newPos <> NULL then
+	begin
+		newPos.data:=d;
+		newPos.next:=NULL;
+	end;
+end;
+
+function insertItem(i:tItem; p:tPosL; L:tList):boolean;
+var
+	newPos,q:tPosL;
+	
+begin
+	createNode(d,newNode);
+	if (newNode=NULL) then
+		insertItem:=false;
+	else
+	begin
+		insertItem:=true;
+		if L=NULL then
+			L:=newPos;
+		else if p = NULL then
+		begin
+			q:=L;
+			while (q^.next <> NULL) do
+				q:=q^.next;
+			q^.next:=newPos;
+		end
+		else
+		begin
+			newPos^.item:p^.item;
+			p^.item:=d;
+			newPos^.next:p^.next;
+			p^.next:=newPos;
+		end;
+	end;
+end;
+
+procedure deleteAtPosition (p:tPosL; VAR L:tList);
+var
+	q:tPosL;
+
+begin
+	if p=L then
+		L:=L^.next;
+	else if p^.next=NULL then
+	begin
+		q:=L;
+		while q^.next <> p
+			q:=q^.next;
+		q^.next:=NULL;
+	end;
+	else
+	begin
+		q:=p`.next;
+		p^.item:=q^.item;
+		p^.next:=q^.next;
+		p:=q;
+	end;
+	dispose(p);
 end;
 
 function getItem(p:tPosL; L:tList): tItem;
@@ -93,20 +159,15 @@ begin
 	getItem:=p^.item;
 end;
 
-procedure updateItem(var l:tList;p:tPosL;i:tItem);
+procedure updateItem(var L:tList;p:tPosL;i:tItem);
 begin
 	p^.item:=i;
 end;
 
 function findItem (ing:tnIngredient; L:tList):tPosL;
 begin
-	if(l^.item.nIngredient = ing) then
+	if(L^.item.nIngredient = ing) then
 		findItem := l
 	else
-		findItem(ing,l^.next);
+		findItem(ing,L^.next);
 end;
-BEGIN
-	
-	
-END.
-
